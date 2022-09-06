92ce4a1dbe1988d5b40f6fa64659a7ce1bf56768
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2022-09-06 15:05:46 -0500

    one more edit to the file

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 
 int main()
 {
+	int example=0;
 	std::cout<<"Hello World"<<"\n";
 	std::cout<<"Hello World"<<"\n";
 	std::cout<<"Hello World"<<"\n";
-
+	std::cout<<"Please enter the number of times you opened this page"<<"\n";
+	std::cin>>example;
+	std::cout<<example<<"\n";
 	std::cout<<"Hello World"<<"\n";
 
 
